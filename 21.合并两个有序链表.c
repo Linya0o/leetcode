1341889d0c19899fcb9ca706556cb8386c97fddc
John <linya0o@163.com>
2022-12-13 16:51:30 +0800

    [21] 双指针

---
+++ 21.合并两个有序链表.c
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=c
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
+    struct ListNode *dummy, *p;
+    dummy = p = calloc(1, sizeof(struct ListNode));
+    while (list1 && list2) {
+        if (list1->val <= list2->val) {
+            p->next = list1;
+            list1 = list1->next;
+        } else {
+            p->next = list2;
+            list2 = list2->next;
+        }
+        p = p->next;
+    }
+    if (list1) p->next = list1;
+    if (list2) p->next = list2;
+    return dummy->next;
+}
+// @lc code=end
+
