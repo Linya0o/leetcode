7bb9fd7f1be03e46d5a367c647d41cdc4e3bbc40
John <linya0o@163.com>
2023-01-06 17:13:14 +0800

    [27] 快慢指针

---
+++ 27.移除元素.c
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=27 lang=c
+ *
+ * [27] 移除元素
+ */
+
+// @lc code=start
+
+
+int removeElement(int* nums, int numsSize, int val){
+    int slow = 0, fast = 0;
+    while (fast < numsSize) {
+        if (nums[fast] != val) {
+            nums[slow++] = nums[fast];
+        }
+        fast++;
+    }
+    return slow;
+}
+// @lc code=end
+
